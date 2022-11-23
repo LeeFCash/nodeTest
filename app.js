db87c2df8215b7337c8849c61c979de6468ded1b
LeeFCash <118697195+LeeFCash@users.noreply.github.com>
2022-11-23 23:02:58 +0000

    change html to ejs
    that allows me to use the backend stuff and front end stuff together

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { dirname } = require('path');
 const path = require('path');
 const app = express();
-app.use('/public', express.static(path.join(__dirname, 'main')));
+
+//app.use('/public', express.static(path.join(__dirname, 'main')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get('/',(req,res)=>{
- res.sendFile(path.join(__dirname,'main','terminal.html'));
+// res.sendFile(path.join(__dirname,'main','terminal.html'));
+res.render("terminal");
 });
 app.listen(5000);
