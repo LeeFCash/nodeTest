74bd98da9a241123210ed41d7a667ac108edb6fd
LeeFCash <118697195+LeeFCash@users.noreply.github.com>
2022-12-05 22:50:16 +0000

    redoing terminal for node js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { dirname } = require('path');
 const path = require('path');
-const pcALL = require('./routes/pcALL');
+// one is the const/var that gets the route1 file 
+const route1 = require('./routes/Route1');
 const app = express();
 
 //app.use('/public', express.static(path.join(__dirname, 'main')));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// adding pcALL to /pcALL
-app.use('/pcALL', pcALL);
+// url is /pcALL use this to go to page / making url
+app.use('/pcALL', route1);
 // show terminal 
 app.get('/',(req,res)=>{
 const testThis = [{
--- routes/Route1.js
+++ routes/Route1.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //adding new page
 router.get('/', (req,res) => {
-res.send('hello this is page 2');
+res.render('pcALL');
 })
 
 module.exports = router;
